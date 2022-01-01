fa983520368faa84af423da37aa40f88583b8815
Saeid Pournejati <pournejati@gmail.com>
2021-12-31 23:09:46 -0800

    Added basics file.

---
+++ com/saeid/main/Basics.java
@@ -0,0 +1,8 @@
+package com.saeid.main;
+
+public class Basics {
+    public static void main(String[] args) {
+        String testMessage = "Test";
+        System.out.println(testMessage);
+    }
+}
