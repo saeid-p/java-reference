80602e4d48473e91104de9d4155c04ccd6ba79b7
Saeid Nejati <pournejati@gmail.com>
2023-01-01 11:49:09 -0800

    Added a test class with simple props.

---
+++ com/saeid/main/TestBaseClass.java
@@ -0,0 +1,20 @@
+/**
+ * You can also document a package.
+ */
+package com.saeid.main;
+
+/**
+ * This is a standard documentation for the class.
+ */
+public class TestBaseClass {
+    private String name;
+
+    public String geName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
